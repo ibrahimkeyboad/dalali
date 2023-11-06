4e40b2a3f1d0515ee58afd1bedd253b39be193ca
ibrahimkeyboad <ibrahimmwanga07@gmail.com>
2023-11-06 18:44:58 +0300

    add sqlite

--- src/sqlite/db.sql
+++ src/sqlite/db.sql
@@ -1,1 +1,4 @@
-CREATE TABLE country 
+CREATE TABLE countries(id  INTEGER PRIMARY KEY, name TEXT)
+
+CREATE TABLE cities(id INTEGER PRIMARY KEY, name TEXT, country_id INTEGER, FOREIGN KEY (country_id) REFERENCES countries(id)
+CREATE TABLE regions(id INTEGER PRIMARY KEY, name TEXT, country_id INTEGER, FOREIGN KEY (country_id) REFERENCES countries(id)
