c55733bb7d53e115c91e72cf84ffff08f6a6dfa5
ibrahimkeyboad <ibrahimmwanga07@gmail.com>
2023-11-06 18:23:32 +0300

    add sql

--- middlewate.ts
+++ middlewate.ts
@@ -1,9 +1,9 @@
 import NextAuth from 'next-auth';
 import { authOptions } from '@/actions/authOptions';
 
-export default NextAuth(authOptions).auth;
+// export default NextAuth(authOptions).auth;
 
 export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
-  matcher: ['/((?!api|_next/static|_next/image|.png).*)'],
+  // matcher: ['/((?!api|_next/static|_next/image|.png).*)'],
 };
