b11c762e9a5098a996cba0e7ac262bdac0f52b7b
ibrahimkeyboad <ibrahimmwanga07@gmail.com>
2023-11-09 16:14:23 +0300

    fix bug

--- src/app/(auth)/verify/phoneNumber/page.tsx
+++ src/app/(auth)/verify/phoneNumber/page.tsx
@@ -1,7 +1,60 @@
-import React from 'react';
+'use client';
 
-function page() {
-  return <div>page</div>;
+import { Form } from '@/components/ui/form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import axios from 'axios';
+import { Metadata } from 'next';
+import { useRouter } from 'next/navigation';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+export const metadata: Metadata = {
+  title: 'Hakikisha namba yako ya simu',
+};
+
+const FormSchema = z.object({
+  firstName: z.string().min(2, {
+    message: 'Username must be at least 2 characters.',
+  }),
+});
+
+type MakeOfferFormValues = z.infer<typeof FormSchema>;
+
+function Page() {
+  const router = useRouter();
+
+  const form = useForm<MakeOfferFormValues>({
+    resolver: zodResolver(FormSchema),
+  });
+
+  async function onSubmitHandler(value: MakeOfferFormValues) {
+    try {
+      const res = await axios.post('/api/auth/fungua-account', value);
+      if (res.status == 201) {
+        router.push('/verify');
+        // toast.success(res.data.msg);
+      } else {
+        // toast.error('fail', res.data);
+      }
+    } catch (errors: any) {
+    } finally {
+      // setIsLoading(false);
+    }
+  }
+
+  return (
+    <div>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmitHandler)}
+          className=' bg-card w-[90%] shadow-lg shadow-gray-200 dark:shadow-none rounded-lg md:w-[70%] lg:w-[45%] px-10 m-auto gap-6 flex flex-col items-stretch border-2 py-4'>
+          <h1 className='text-3xl font-semibold self-center  text-primary'>
+            Create Your Account
+          </h1>
+        </form>
+      </Form>
+    </div>
+  );
 }
 
-export default page;
+export default Page;
--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,13 +1,6 @@
-import { NextResponse } from 'next/server';
-
 export async function GET(req: Request) {
   try {
-    let total = 0;
-    for (let i = 0; i < 50000; i++) {
-      total++;
-      console.log(total);
-    }
-    return Response.json('hello' + ' ' + total);
+    return Response.json('hello');
   } catch (err) {
     new Response('something went wrong', { status: 500 });
   }
