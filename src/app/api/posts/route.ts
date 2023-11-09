e7807f7c6fa09ceec15042af5d9fa39208c37d6a
ibrahimkeyboad <ibrahimmwanga07@gmail.com>
2023-11-09 15:21:35 +0300

    thread

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -2,7 +2,12 @@ import { NextResponse } from 'next/server';
 
 export async function GET(req: Request) {
   try {
-    return Response.json('hello');
+    let total = 0;
+    for (let i = 0; i < 50000; i++) {
+      total++;
+      console.log(total);
+    }
+    return Response.json('hello' + ' ' + total);
   } catch (err) {
     new Response('something went wrong', { status: 500 });
   }
