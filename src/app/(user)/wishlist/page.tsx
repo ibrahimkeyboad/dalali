9b50ff6e62fc6d71c8c4e89765f3d56ffddc6055
ibrahimkeyboad <ibrahimmwanga07@gmail.com>
2024-02-01 00:51:37 +0300

    fix bug in wish list

--- src/app/(user)/wishlist/page.tsx
+++ src/app/(user)/wishlist/page.tsx
@@ -1,3 +1,4 @@
+import { MountainIcon } from 'lucide-react';
 import Link from 'next/link';
 
 export default function Component() {
@@ -158,23 +159,5 @@ export default function Component() {
         </nav>
       </footer>
     </div>
-  );
-}
-
-function MountainIcon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns='http://www.w3.org/2000/svg'
-      width='24'
-      height='24'
-      viewBox='0 0 24 24'
-      fill='none'
-      stroke='currentColor'
-      strokeWidth='2'
-      strokeLinecap='round'
-      strokeLinejoin='round'>
-      <path d='m8 3 4 8 5-5 5 15H2L8 3z' />
-    </svg>
   );
 }
